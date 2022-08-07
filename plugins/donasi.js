8c7a7f6440b388e026b1c82268be8f2664190951
JAYBOTz <106862955+EkaBotWhatsapp@users.noreply.github.com>
2022-08-07 21:58:58 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,5 @@
 let handler = async (m, { conn }) => {
-	conn.sendButtonImg(m.chat, 'https://b.top4top.io/p_2407l281d2.jpg', 'Nih Donate Via Ovonya', wm, 'MENU', '.menu', m)
-	conn.sendButtonImg(m.chat, 'https://c.top4top.io/p_2407hnpgj3.jpg', 'Nih Donate Via Dananya', wm, 'MENU', '.menu', m)
-	conn.sendButtonImg(m.chat, 'https://d.top4top.io/p_2407znd8a4.jpg', 'Nih Donate Via Gopaynya', wm, 'MENU', '.menu', m)
+	conn.sendButtonImg(m.chat, 'https://f.top4top.io/p_2410vt78j1.jpg', 'Nih Donatenya', wm, 'MENU', '.menu', m)
 }
 handler.help = ['donate']
 handler.tags = ['info']
