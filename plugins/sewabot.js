182d6c3c987e6b19423f6bba5ec9f211ced0dca2
JAYBOTz <106862955+EkaBotWhatsapp@users.noreply.github.com>
2022-08-07 10:39:19 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
-	conn.sendButtonImg(m.chat, 'https://telegra.ph/file/0f39992221e43a5005621.jpg', 'Nih List Harganya', wm, 'Payment', '.Payment', m)
+	conn.sendButtonImg(m.chat, 'https://a.top4top.io/p_2407o2ddm1.jpg', 'Nih List Harganya', wm, 'Payment', '.Payment', m)
 }
 handler.help = ['sewa']
 handler.tags = ['main']
 
 handler.command = /^(sewa)$/i
-handler.register = true
+
 handler.limit = 1
 module.exports = handler
