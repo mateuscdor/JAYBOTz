a52409e367819fe9666db1c9628c6369d86de2a7
JAYBOTz <106862955+EkaBotWhatsapp@users.noreply.github.com>
2022-08-07 10:42:19 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
-	conn.sendButtonImg(m.chat, 'https://telegra.ph/file/604ae8b3b6ec6c8bc3a28.jpg', 'Nih Donate Via Ovonya', wm, 'MENU', '.menu', m)
-	conn.sendButtonImg(m.chat, 'https://telegra.ph/file/e78245fb9a9ea9d370449.jpg', 'Nih Donate Via Dananya', wm, 'MENU', '.menu', m)
-	conn.sendButtonImg(m.chat, 'https://telegra.ph/file/70ce6d52c3fa786a297b6.jpg', 'Nih Donate Via Gopaynya', wm, 'MENU', '.menu', m)
+	conn.sendButtonImg(m.chat, 'https://b.top4top.io/p_2407l281d2.jpg', 'Nih Donate Via Ovonya', wm, 'MENU', '.menu', m)
+	conn.sendButtonImg(m.chat, 'https://c.top4top.io/p_2407hnpgj3.jpg', 'Nih Donate Via Dananya', wm, 'MENU', '.menu', m)
+	conn.sendButtonImg(m.chat, 'https://d.top4top.io/p_2407znd8a4.jpg', 'Nih Donate Via Gopaynya', wm, 'MENU', '.menu', m)
 }
 handler.help = ['donate']
 handler.tags = ['info']
